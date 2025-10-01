037373463f7561cbd69995fd12ae004aa3e00f17
NumsRan <iavoran24@gmail.com>
2025-10-01 11:14:48 +0200

    feat(task): add task description design

--- src/components/child-tasks-managment/task.vue
+++ src/components/child-tasks-managment/task.vue
@@ -1,15 +1,34 @@
 <template>
     <div class="task">
-        Training Vue.js
+        <h1 class="task-title">Training Vue.js</h1>
+        <p class="task-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam facere corporis at ea est magni repudiandae earum</p>
     </div>
 </template>
 
 <style scoped>
     .task {
         box-shadow: #bababa 3px 2px 4px;
+        border: .5px solid #fefefe;
         background-color: #fefefe;
-        border-radius: 2px;
         margin-bottom: 16px;
+        border-radius: 2px;
         padding: 8px;
+    }
+
+    .task:hover {
+        border: .5px  solid #78fff2;
+        box-shadow: none;
+        cursor: pointer;
+    }
+
+    .task-title {
+        font-family: 'Roboto-Bold';
+        margin-bottom: 8px;
+        font-size: 16px;
+    }
+
+    .task-description {
+        font-family: 'Roboto-Medium';
+        font-size: 12px;
     }
 </style>
